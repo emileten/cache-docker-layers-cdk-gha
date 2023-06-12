5856e4e30ef9be51fe9875d34d5d4087892ba9d7
emileten <e.tenezakis@gmail.com>
2023-06-12 15:05:51 +0900

    try using github cache

--- .github/workflows/scripts/cdk_docker.sh
+++ .github/workflows/scripts/cdk_docker.sh
@@ -4,8 +4,8 @@
 if [[ $1 == "build" ]]; then
   # Use docker buildx for build commands
   # echo the command before running it
-  echo docker buildx "${@:1}" --load
-  command docker buildx "${@:1}" --load
+  echo docker buildx "${@:1}" --load --cache-to "type=gha,mode=max" --cache-from type=gha
+  command docker buildx "${@:1}" --load --cache-to "type=gha,mode=max" --cache-from type=gha
 else
   # Use docker for all other commands
   # avoid echoing the command because the CDK relies on sdout structure.
