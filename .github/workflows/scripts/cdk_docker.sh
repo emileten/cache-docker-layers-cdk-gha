679f50cf062ddecfbc4041d1961acf50b6437671
emileten <e.tenezakis@gmail.com>
2023-06-12 14:52:15 +0900

    avoid messing up docker create stdout

--- .github/workflows/scripts/cdk_docker.sh
+++ .github/workflows/scripts/cdk_docker.sh
@@ -6,14 +6,8 @@ if [[ $1 == "build" ]]; then
   # echo the command before running it
   echo docker buildx "${@:1}" --load
   command docker buildx "${@:1}" --load
-elif [[ $1 == "cp" ]]; then
-  # Add a 'cdk-' prefix to the first of all the remaining arguments
-  # Echo the command before running it
-  echo docker cp "cdk-${2}" "${@:3}"
-  command docker cp "cdk-${2}" "${@:3}"
 else
   # Use docker for all other commands
-  # echo the command before running it
-  echo docker "${@:1}"
+  # avoid echoing the command because the CDK relies on sdout structure.
   command docker "${@:1}"
 fi
