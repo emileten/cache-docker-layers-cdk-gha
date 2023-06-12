44d2d503c2755391fb1b961cd119a9fba125cf1b
emileten <e.tenezakis@gmail.com>
2023-06-12 14:41:42 +0900

    tweak cp

--- .github/workflows/scripts/cdk_docker.sh
+++ .github/workflows/scripts/cdk_docker.sh
@@ -6,6 +6,11 @@ if [[ $1 == "build" ]]; then
   # echo the command before running it
   echo docker buildx "${@:1}" --load
   command docker buildx "${@:1}" --load
+elif [[ $1 == "cp" ]]; then
+  # Add a 'cdk-' prefix to the first of all the remaining arguments
+  # Echo the command before running it
+  echo docker cp "cdk-${2}" "${@:3}"
+  command docker cp "cdk-${2}" "${@:3}"
 else
   # Use docker for all other commands
   # echo the command before running it
